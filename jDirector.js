5cade790302042527211fa0cdf69a3acb1cc08af
ladace <ladacez@gmail.com>
2013-04-04 10:45:24 +0800

    add addInstantFuncs and addContinuous Funcs

--- jDirector.js
+++ jDirector.js
@@ -1,62 +1,91 @@
-function Director() {
-    var director = this;
-    function Future(obj) {
-        var actions = [];
+function Future(obj) {
+    var actions = [];
 
-        function instantPort(func) {
-            return function () {
-                var args = arguments;
-                actions.push(function () {
-                    func.apply(obj, args);
-                });
-                return this;
-            }
-        }
-        function continuousPort(func) {
-            return function () {
-                var future = new Future(obj);
-                var args = arguments;
-                actions.push(function () {
-                    future.follow(func.apply(this, args));
-                });
-                return future;
-            }
-        }
-
-        var thisFuture = this;
-        function portAll(insFuncNames, conFuncNames) {
-            insFuncNames.forEach(function (name) {
-                thisFuture[name] = instantPort(obj[name]);
-            });
-            conFuncNames.forEach(function (name) {
-                thisFuture[name] = continuousPort(obj[name]);
-            });
-        }
-
-        portAll(["log"], ["delay"]);
-
-        // future manipulations
-        this.schedule = function(obj) {
-            actions.forEach(function(a) {
-                a.call(obj);
-            });
-        }
-        this.onSchedule = function (callback) {
-            var that = this;
+    function instantPort(func) {
+        return function () {
+            var args = arguments;
             actions.push(function () {
-                callback.call(that, this);
+                func.apply(obj, args);
             });
+            return this;
         }
-        this.follow = function (prev) {
-            var that = this;
-            prev.onSchedule(function (obj) {
-                that.schedule(obj);
+    }
+    function continuousPort(func) {
+        return function () {
+            var future = new Future(obj);
+            var args = arguments;
+            actions.push(function () {
+                future.follow(func.apply(this, args));
             });
+            return future;
         }
     }
 
-    this.draw = function () {};
-    this.setAnimation = function (callback) {
+    var thisFuture = this;
+    function portAll(insFuncNames, conFuncNames) {
+        insFuncNames.forEach(function (name) {
+            thisFuture[name] = instantPort(obj[name]);
+        });
+        conFuncNames.forEach(function (name) {
+            thisFuture[name] = continuousPort(obj[name]);
+        });
+    }
+
+    portAll(obj.instantFuncs, obj.continuousFuncs);
+
+    // future manipulations
+    this.schedule = function(obj) {
+        actions.forEach(function(a) {
+            a.call(obj);
+        });
+    }
+    this.onSchedule = function (callback) {
+        var that = this;
+        actions.push(function () {
+            callback.call(that, this);
+        });
+    }
+    this.follow = function (prev) {
+        var that = this;
+        prev.onSchedule(function (obj) {
+            that.schedule(obj);
+        });
+    }
+}
+
+function Director() {
+    this.instantFuncs = [];
+    this.continuousFuncs = [];
+
+    // Animation Manipulation
+    this.addInstantFunc = function (name, callback) {
+        if (this[name]) return false;
+        this[name] = callback;
+        this.instantFuncs.push(name);
+        return true;
+    }
+    this.addContinuousFunc = function (name, callback) {
+        if (this[name]) return false;
+        this[name] = callback;
+        this.continuousFuncs.push(name);
+        return true;
+    }
+
+    this.addInstantFunc("log", function () {
+        console.log.apply(console, arguments);
+        return this;
+    });
+    
+    this.addContinuousFunc("delay", function (ms) {
+        var that = this;
+        var future = new Future(this);
+        setTimeout(function() {
+            future.schedule(that);
+        }, ms);
+        return future;
+    });
+
+    this.addContinuousFunc("animate", function (callback) {
         var that = this;
         var future = new Future(this);
         var cid = setInterval(function() {
@@ -64,30 +93,10 @@ function Director() {
                 clearInterval(cid);
                 future.schedule(this);
             }
-        }, 20);
+        }, 20); // TODO Make it configurable
         return future;
-    }
-
-
-    // animation functions
-    this.log = function () {
-        console.log.apply(console, arguments);
-        return this;
-    }
-    this.delay = function (ms) {
-        var that = this;
-        var future = new Future(this);
-        setTimeout(function() {
-            future.schedule(that);
-        }, ms);
-        return future;
-    }
-    /*this.fadeOut = function () {
-        return this.setAnimation(function () {
-            return true; // terminate
-        });
-    }*/
+    });
 }
 
 director = new Director();
-director.delay(1000).log("OK").delay(2000).log("Ah").delay(5000).log("fuck").delay(1000).log("en");
+director.delay(1000).log("OK").delay(1000).log("Ah").delay(2000).log("Oops").delay(1000).log("Hah");
